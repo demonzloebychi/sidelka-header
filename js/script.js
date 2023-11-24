1b1ff2f358642d4608e414bcfac8403f5b333ae8
Nikolai <107766413+demonzloebychi@users.noreply.github.com>
2023-11-25 01:58:59 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.menu');
+// const link = document.querySelector('.header-main__item')
+
+
+if (burger){
+    burger.addEventListener("click", function(e){
+        document.body.classList.toggle('lock');
+        burger.classList.toggle('active');
+        menu.classList.toggle('active');
+    });
+
+    // menu.querySelectorAll('.header-main__item').forEach(link => {
+    //     link.addEventListener('click', function(e) {
+    //         document.body.classList.remove('lock');
+    //         menu.classList.remove('active');
+    //         burger.classList.remove('active');
+    //     })
+    // })
+    
+}
+
+let body = document.querySelector('body');
+body.classList.add('touch');
+let arrow = document.querySelectorAll('.arrow');
+for(i=0;i<arrow.length; i++){
+let thisLink = arrow[i].previousElementSibling;
+let subMenu = arrow[i].nextElementSibling;
+let thisArrow = arrow[i];
+
+thisLink.classList.add('parent');
+arrow[i].addEventListener('click', function(){
+subMenu.classList.toggle('open');
+thisArrow.classList.toggle('active');
+});
+};
